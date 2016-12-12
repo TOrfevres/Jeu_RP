3d08fba6648699611018358ce8069110bb56aff2
Killian <killian.barreau@ynov.com>
2016-12-12 16:59:29 +0100

    Adding Clés

---
+++ src/com/environment/Clés.java
@@ -0,0 +1,16 @@
+package com.environment;
+
+/**
+ * Created by Killian on 12/12/2016.
+ */
+public class Clés {
+    private String passageAssocie;
+
+    public Clés(String passageAssocie){
+        this.passageAssocie = passageAssocie;
+    }
+
+    public String getPassageAssocie(){
+        return this.passageAssocie;
+    }
+}
