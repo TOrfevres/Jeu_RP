8679bb15026e29c61d0645899d6cadf05b0e394b
T.Orfèvres <theodore.orfevres@ynov.com>
2016-12-12 17:02:20 +0100

    Testing things

--- src/com/environment/Consommables.java
+++ src/com/environment/Consommables.java
@@ -3,11 +3,9 @@ package com.environment;
 /**
  * Created by user on 12/12/2016.
  */
-public class Consommables extends Objets{
-    private int nombre;
+public class Consommables extends Objets implements Obtenables{
 
     public Consommables(String nom, Types type, String description, Pieces piece) {
         super(nom, type, description, piece);
-        this.nombre = 0;
     }
 }
