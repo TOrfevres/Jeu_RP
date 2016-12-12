6244460dceccd36e5f7fec080b10a32fecb17bae
Killian <killian.barreau@ynov.com>
2016-12-12 16:41:02 +0100

    Defining Objets and creating enum Type

--- src/com/environment/Objets.java
+++ src/com/environment/Objets.java
@@ -3,6 +3,33 @@ package com.environment;
 /**
  * Created by user on 12/12/2016.
  */
-public class Objets {
+public abstract class Objets {
+    private String nom;
+    private Type type;
+    private String description;
+    private Pieces piece;
 
+    public Objets(String nom, Type type, String description, Pieces piece){
+        this.nom = nom;
+        this.type = type;
+        this.description = description;
+        this.piece = piece;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public Type getType(){
+        return this.type;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public Pieces getPiece(){
+        return this.piece;
+    }
 }
+
---
+++ src/com/environment/Type.java
@@ -0,0 +1,10 @@
+package com.environment;
+
+/**
+ * Created by Killian on 12/12/2016.
+ */
+public enum Type {
+    Inforamatif,
+    Obtenable,
+    Clés;
+}
