52ae85ce166af309824920f773e79f946de5414d
Kirian <kirian.caumes@ynov.com>
2016-12-13 09:42:42 +0100

    salut les pd

--- src/com/environment/Objets/Armes.java
+++ src/com/environment/Objets/Armes.java
@@ -5,7 +5,7 @@ import com.main.Main;
 /**
  * Created by user on 12/12/2016.
  */
-public class Armes extends Objets {
+public class Armes extends Objets { //penis
     private int degats;
 
     public Armes(String nom, String type, String description, String piece, int degats) {
