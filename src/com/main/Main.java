3016818f55c553e2685d99be73080979c8b09bb7
Kirian <kirian.caumes@ynov.com>
2016-12-13 14:23:22 +0100

    oui6

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,15 +4,13 @@ import com.entities.Joueurs;
 import com.environment.Pieces;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Scanner;
 
-
-/**
- * Created by user on 12/12/2016.
- */
 public class Main {
     public static Collection<Pieces> niveau = new Collection<Pieces>() {
         @Override
@@ -88,9 +86,8 @@ public class Main {
     static boolean retry = true;
 
     public static void main(String[] args) {
-
-        FileReader fr;
-
+        System.out.println("  << JMistery >>");
+        System.out.println("Création d'un nouveau scénario...");
 
         selectionMonde();
         selectionPersonnage();
@@ -185,7 +182,7 @@ public class Main {
             String cheminMonstre = "";
             String descriptionJeu = "";
             String cheminSimple = "";
-            String ligne = "";
+
             //boolean retry = true;
             //Scanner sc;
             System.out.println("Saisissez l'emplacement du fichier de description du niveau pour ce Scénario : ");
@@ -212,30 +209,9 @@ public class Main {
                 cheminObjet = cheminSimple + cheminObjet;
                 cheminMonstre = cheminSimple + cheminMonstre;
 
-                /*System.out.println(cheminMonstre);
-                System.out.println(cheminObjet);
-                System.out.println(cheminCarte);
-                System.out.println(cheminSimple);*/
-
-                BufferedReader monstre = new BufferedReader(new FileReader(cheminMonstre));
-                BufferedReader objet = new BufferedReader(new FileReader(cheminObjet));
-                BufferedReader carte = new BufferedReader(new FileReader(cheminCarte));
-
-
-                while ((ligne = carte.readLine()) != null) {
-                    if (ligne.equals("<")) {
-                        String nom = carte.readLine();
-                        nom = nom.substring(1, (nom.length() - 1));       // recup nom pieces
-
-                        carte.readLine();   //saute une ligne
-
-                        String desc = carte.readLine();                     //recup toutes la desc
-                        while (!(ligne = carte.readLine()).equals("\\")) {
-                            desc = desc + "\r\n" + ligne;
-                        }
-                        niveau.add(new Pieces(nom, desc));              //creee l'entitee
-                    }
-                }
+                chargementMonstre(cheminMonstre);
+                chargementObjet(cheminObjet);
+                chargementCarte(cheminCarte);
 
                 retry = false;
             } catch (Exception FileNotFoundException) { // Rajouter des throw ????
@@ -255,4 +231,42 @@ public class Main {
         //Appliquer la méthode "setSorties()" à l'objet
     }
 
+    public static void chargementMonstre(String chemin){
+        try {
+            BufferedReader monstre = new BufferedReader(new FileReader(chemin));
+        } catch (FileNotFoundException e) {
+            System.out.println("Le nom du fichier pour les monstres spécifié dans votre texte Niveau est inchorect.");
+        }
+    }
+
+    public static void chargementObjet(String chemin){
+        try {
+            BufferedReader objet = new BufferedReader(new FileReader(chemin));
+        } catch (FileNotFoundException e) {
+            System.out.println("Le nom du fichier pour les objets spécifié dans votre texte Niveau est inchorect.");
+        }
+    }
+
+    public static void chargementCarte(String chemin){
+        String ligne = "";
+        try {
+            BufferedReader carte = new BufferedReader(new FileReader(chemin));
+            while ((ligne = carte.readLine()) != null) {
+                if (ligne.equals("<")) {
+                    String nom = carte.readLine();
+                    nom = nom.substring(1, (nom.length() - 1));       // recup nom pieces
+
+                    carte.readLine();   //saute une ligne
+
+                    String desc = carte.readLine();                     //recup toutes la desc
+                    while (!(ligne = carte.readLine()).equals("\\")) {
+                        desc = desc + "\r\n" + ligne;
+                    }
+                    niveau.add(new Pieces(nom, desc));              //creee l'entitee
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Le nom du fichier pour les cartes spécifié dans votre texte Niveau est inchorect.");
+        }
+    }
 }
