9242392f51a3ce4252c4b8908bf48e223c5571c1
T.Orfèvres <theodore.orfevres@ynov.com>
2016-12-13 12:03:29 +0100

    Testing things

--- src/com/entities/Monstres.java
+++ src/com/entities/Monstres.java
@@ -6,20 +6,20 @@ import com.environment.Pieces;
  * Created by user on 12/12/2016.
  */
 public class Monstres extends Personnages {
-    public Monstres(String name, int pointsVie, int pointsAttaque, int pointsDefense, Pieces pieceActuelle) {
+    public Monstres(String name, int pointsVie, int pointsAttaque, Pieces pieceActuelle) {
         //Si le monstre n'est pas un boss, set les points de défense à 0, sinon set à une valeur proche à celle du joueur
-        super(name, pointsVie, pointsAttaque, pointsDefense, pieceActuelle);
+        super(name, pointsVie, pointsAttaque, pieceActuelle);
     }
 
-    public void autoAttaque(Joueurs joueur) {
+    public void attaque(Joueurs joueur) {
         if(joueur.getPieceActuelle().getNom() == this.getPieceActuelle().getNom() && this.getPointsVie() > 0) {
             int defenseAleatoire = Math.toIntExact(Math.round(Math.random() * joueur.getPointsDefense()));
             int degatsNouvelleAttaque = this.getPointsAttaque() - defenseAleatoire;
             if(degatsNouvelleAttaque <= 0) {
-                System.out.println("Vous venez d'esquiver l'attaque venant de " + this.getName() + " !");
+                System.out.println("Vous venez d'esquiver l'attaque venant de " + this.getNom() + " !");
             } else if (degatsNouvelleAttaque > 0) {
                 joueur.setPointsVie(joueur.getPointsVie() - degatsNouvelleAttaque);
-                System.out.println(this.getName() + " vient de vous attaquer et de vous infliger " + this.getPointsAttaque() + " points de dégats.");
+                System.out.println(this.getNom() + " vient de vous attaquer et de vous infliger " + this.getPointsAttaque() + " points de dégats.");
             }
         }
     }
